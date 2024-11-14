0e76decfbb3971767cfc0a096a46e2870551d40c
rookie-leo <proleonardopa@gmail.com>
2024-11-14 15:34:53 -0300

    Repositories e Services para as entidades

---
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.repositories.ModuleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModuleServiceImpl {
+
+    final ModuleRepository moduleRepository;
+
+    public ModuleServiceImpl(ModuleRepository moduleRepository) {
+        this.moduleRepository = moduleRepository;
+    }
+}
