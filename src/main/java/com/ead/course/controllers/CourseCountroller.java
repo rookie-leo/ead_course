83b4a1faf8a1d3c75f6f6842407b1db6b83d354f
rookie-leo <proleonardopa@gmail.com>
2024-11-15 15:35:35 -0300

    Implementado endpoint para cadastro de curso

---
+++ src/main/java/com/ead/course/controllers/CourseCountroller.java
@@ -0,0 +1,30 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.CourseRecordDto;
+import com.ead.course.services.CourseService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseCountroller {
+
+    final CourseService courseService;
+
+    public CourseCountroller(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseRecordDto courseRecordDto) {
+        if (courseService.existsByName(courseRecordDto.name()))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Course Name is Already Taken!");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseRecordDto));
+    }
+}
