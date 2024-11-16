a6be7cec745a3580d0b233729efd364c45bb8ee4
rookie-leo <proleonardopa@gmail.com>
2024-11-16 17:54:28 -0300

    Implementado CRUD para modulos

--- src/main/java/com/ead/course/controllers/ModuleController.java
+++ src/main/java/com/ead/course/controllers/ModuleController.java
@@ -1,16 +1,15 @@
 package com.ead.course.controllers;
 
 import com.ead.course.dtos.ModuleRecordDto;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.ModuleService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,9 +25,44 @@ public class ModuleController {
 
     @PostMapping("/courses/{courseId}/modules")
     public ResponseEntity<Object> saveModule(
-            @PathVariable(value = "courseId")UUID courseId,
+            @PathVariable(value = "courseId") UUID courseId,
             @RequestBody @Valid ModuleRecordDto moduleRecordDto
-            ) {
-        return ResponseEntity.status(HttpStatus.OK).body(moduleService.save(moduleRecordDto, courseService.findById(courseId).get()));
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                moduleService.save(moduleRecordDto, courseService.findById(courseId).get())
+        );
+    }
+
+    @GetMapping("/courses/{courseId}/modules")
+    public ResponseEntity<List<ModuleModel>> getAllModels(@PathVariable(value = "courseId") UUID courseId) {
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllModulesIntoCourse(courseId));
+    }
+
+    @GetMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> getOneModel(
+            @PathVariable(value = "courseId") UUID courseId,
+            @PathVariable(value = "moduleId") UUID moduleId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findModuleIntoCourse(courseId, moduleId));
+    }
+
+    @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> deleteModule(
+            @PathVariable("courseId") UUID courseId,
+            @PathVariable(value = "moduleId") UUID moduleId
+    ) {
+        moduleService.delete(moduleService.findModuleIntoCourse(courseId, moduleId).get());
+        return ResponseEntity.status(HttpStatus.OK).body("Module deleted successfully");
+    }
+
+    @PutMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> updateModule(
+            @PathVariable("courseId") UUID courseId,
+            @PathVariable(value = "moduleId") UUID moduleId,
+            @RequestBody @Valid ModuleRecordDto moduleRecordDto
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                moduleService.update(moduleRecordDto, moduleService.findModuleIntoCourse(courseId, moduleId).get())
+        );
     }
 }
