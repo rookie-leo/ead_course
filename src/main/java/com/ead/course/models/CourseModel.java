4dde42e1e948182f80737841d512e7cc2f127d59
rookie-leo <proleonardopa@gmail.com>
2024-11-21 14:25:43 -0300

    Implementado mapeamento entre ourseId e userId

--- src/main/java/com/ead/course/models/CourseModel.java
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -60,4 +60,8 @@ public class CourseModel extends RepresentationModel<CourseModel> implements Ser
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Fetch(FetchMode.SUBSELECT)
     private Set<ModuleModel> modules;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private Set<CourseUserModel> coursesUsers;
 }
---
+++ src/main/java/com/ead/course/models/CourseUserModel.java
@@ -0,0 +1,26 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_COURSES_USERS")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CourseUserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private CourseModel course;
+}
