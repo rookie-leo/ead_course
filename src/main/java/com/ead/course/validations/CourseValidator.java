17bcb07916903b86e8386659b8277bf1f856fc3f
rookie-leo <proleonardopa@gmail.com>
2025-01-11 15:32:53 -0300

    Implementado authorization roles para instructo e admin

--- src/main/java/com/ead/course/validations/CourseValidator.java
+++ src/main/java/com/ead/course/validations/CourseValidator.java
@@ -1,11 +1,15 @@
 package com.ead.course.validations;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
+import com.ead.course.configs.security.UserDetailsImpl;
 import com.ead.course.dtos.CourseRecordDto;
 import com.ead.course.models.UserModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -20,11 +24,13 @@ import static com.ead.course.enums.UserType.USER;
 @Component
 public class CourseValidator implements Validator {
 
+    final AuthenticationCurrentUserService authenticationCurrentUserService;
     private final Validator validator;
     final CourseService courseService;
     final UserService userService;
 
-    public CourseValidator(@Qualifier("defaultValidator") Validator validator, CourseService courseService, UserService userService) {
+    public CourseValidator(AuthenticationCurrentUserService authenticationCurrentUserService, @Qualifier("defaultValidator") Validator validator, CourseService courseService, UserService userService) {
+        this.authenticationCurrentUserService = authenticationCurrentUserService;
         this.validator = validator;
         this.courseService = courseService;
         this.userService = userService;
@@ -54,12 +60,18 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        Optional<UserModel> userModelOptional = userService .findById(userInstructor);
-        var userType = userModelOptional.get().getUserType();
+        UserDetailsImpl userDetails = authenticationCurrentUserService.getCurrentUser();
 
-        if (userType.equals(STUDENT.toString()) || userType.equals(USER.toString())) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN");
-            log.error("Error validation userInstructor: {}", userInstructor);
+        if (userDetails.getUserId().equals(userInstructor) || userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+            Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+            var userType = userModelOptional.get().getUserType();
+
+            if (userType.equals(STUDENT.toString()) || userType.equals(USER.toString())) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN");
+                log.error("Error validation userInstructor: {}", userInstructor);
+            }
+        } else {
+            throw new AccessDeniedException("Forbidden");
         }
     }
 }
